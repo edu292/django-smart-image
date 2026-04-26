7f03bee9852e405f18854f3ad110af711e0cbc8b
edu292 <eduskoroboatei@gmail.com>
2026-04-25 22:34:10 -0300

    Added support for customizing captured image size

--- src/smart_image/widgets.py
+++ src/smart_image/widgets.py
@@ -22,5 +22,17 @@ class PreviewImageInput(FileInput):
 class CamImageInput(FileInput):
     template_name = 'smart_image/cam_image_input.html'
 
+    def __init__(self, capture_width=1920, capture_height=1080, attrs=None):
+        self.capture_width = capture_width
+        self.capture_height = capture_height
+        super().__init__(attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget']['capture_width'] = self.capture_width
+        context['widget']['capture_height'] = self.capture_height
+
+        return context
+
     def format_value(self, value):
         return value
